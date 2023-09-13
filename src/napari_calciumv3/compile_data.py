4e21e15b447ba86bf1a652e9485414beffca1dae
annayang26 <98043434+annayang26@users.noreply.github.com>
2023-09-13 16:31:02 -0400

    fix?

--- src/napari_calciumv3/compile_data.py
+++ src/napari_calciumv3/compile_data.py
@@ -69,6 +69,8 @@ def compile_data(base_folder, file_name="summary.txt",
                         print("value list: ", values)
                         print("value[0]: ", values[0])
 
+                        if values[0] == "N/A":
+                            values[0] = 0
                         data[var] = float(values[0])
 
                         # for item in items:
