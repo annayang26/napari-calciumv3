e9b68dcda60d9f3de631128d09e54cb97517f0e0
annayang26 <98043434+annayang26@users.noreply.github.com>
2023-10-04 09:56:11 -0400

    try mapping roi dict to a set of tuples

--- src/napari_calciumv3/_calcium.py
+++ src/napari_calciumv3/_calcium.py
@@ -1353,7 +1353,7 @@ class Calcium(QWidget):
 
                 # to group the cells in the stimulated area vs not in the stimulated area
                 if self.label_layer:
-                    st_rois, nst_rois = self.group_st_cells(st_area_pos, 0.1)
+                    self.group_st_cells(st_area_pos, 0.1)
                     # spike_templates_file = 'spikes.json'
                     # stimulated cells
                     # roi_signal_st = self.calculate_ROI_intensity(st_rois, self.img_stack)
